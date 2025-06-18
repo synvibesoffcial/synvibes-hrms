101a25322219d9e379d535b56d557e3b24a23118
Neston Cabral <cabralneston@gmail.com>
2025-06-18 17:37:58 +0530

    project rebuild after clerk auth removal

---
+++ prisma/migrations/20250617102249_attendance_with_geolocation/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Attendance" ADD COLUMN     "checkInLatitude" DOUBLE PRECISION,
+ADD COLUMN     "checkInLongitude" DOUBLE PRECISION,
+ADD COLUMN     "checkOutLatitude" DOUBLE PRECISION,
+ADD COLUMN     "checkOutLongitude" DOUBLE PRECISION;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,7 @@
-// import { checkRole } from "../../utils/roles";
-import { redirect } from "next/navigation";
-import { checkAndCreateUser } from "@/lib/checkuser";
-import prisma from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
+
 
 export default async function Home() {
-  await checkAndCreateUser();
-  const { sessionClaims } = await auth();
-  const userId = sessionClaims?.sub;
 
-  if (!userId) {
-    // if not signed in, show sign in or redirect as needed
-    return (
-      <div className="flex min-h-screen flex-col items-center justify-center">
-          <h1 className="text-xl font-bold">
-            Welcome to HRMS
-          </h1>
-          <h1 className="text-3xl font-bold text-yellow-500">
-            Please sign in
-          </h1>
-      </div>
-    );
-  }
-
-  // if role alloted then redirect to the respective page
-  const user = await prisma.user.findUnique({ where: { id: userId } });
-
-  if (user?.role === "hr") {
-    redirect("/hr");
-  } else if (user?.role === "employee") {
-    redirect("/employee");
-  } else if (user?.role === "admin") {
-    redirect("/admin");
-  }
-
-  // If no role, show waiting message
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-3xl font-bold text-yellow-500">
