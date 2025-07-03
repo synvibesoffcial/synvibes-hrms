20394d6a7743f9acec02a923f5d40b5c7a319d4b
Neston Cabral <cabralneston@gmail.com>
2025-07-03 16:17:24 +0530

    fix/ email-error-fixed

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
+    pass: process.env.SMTP_PASS,
   },
 });
 
